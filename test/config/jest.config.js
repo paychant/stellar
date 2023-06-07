669b8aa18b9909c78e9196e0c2dd93659273a6e9
Gabriel Adeniji <31773076+gabrieladeniji@users.noreply.github.com>
2023-06-07 16:30:06 +0100

    Development (#2)
    * refactor: use env TEST_TIMEOUT
    
    * ci: added main.yml workflow

--- test/config/jest.config.js
+++ test/config/jest.config.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
 module.exports = {
   preset: 'ts-jest',
   rootDir: '../../',
   testRegex: '.test.ts$',
   reporters: ['default'],
-  testTimeout: 10000,
+  testTimeout: parseInt(process.env.TEST_TIMEOUT),
   testEnvironment: 'node',
   collectCoverage: false,
   testPathIgnorePatterns: ['<rootDir>/(node_modules)/'],
